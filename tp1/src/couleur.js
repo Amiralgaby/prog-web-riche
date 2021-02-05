b018e79db1efc32a2d54f6d2a44095813c6f4cf1
Gabriel THEUWS <Gabriel.THEUWS@etu.uca.fr>
2021-02-05 15:57:20 +0100

    init repo

---
+++ tp1/src/couleur.js
@@ -0,0 +1,5 @@
+const couleur_rouge = '#ff0000';
+
+const couleur_vert = '#10b020';
+
+const couleur_bleu = '#0004ff';
---
+++ tp1/src/log_message.js
@@ -0,0 +1,9 @@
+function logMessage(message)
+{
+	console.log(message);
+}
+
+function logArray(array_object)
+{
+	array_object.forEach(element => console.log(element));
+}
