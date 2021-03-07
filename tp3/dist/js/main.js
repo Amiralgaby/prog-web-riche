732f440b756287ce523a9f4d1ec8948056e8150f
Amiralgaby <gaby.theuws@gmail.com>
2021-03-07 18:09:52 +0100

    [DEV] mise en place du système d'onglet

--- tp3/dist/js/main.js
+++ tp3/dist/js/main.js
@@ -26,3 +26,55 @@ articles.forEach(function(element){
 	let a = new Article(element.id, element.title, element.description);
 	a.addArticle();
 });
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2,'0');
+var mm = String(today.getMonth() + 1).padStart(2,'0');
+var yyyy = today.getFullYear();
+
+today = yyyy + '-' + dd + '-' +mm;
+today = '2021-03-03';
+console.log(today);
+
+$.ajax({
+    url: 'http://newsapi.org/v2/everything?q=minecraft&from='+ today +'&sortBy=publishedAt&apiKey=9b4e42c6404c48cab0f04cb200654d6c&pageSize=5',
+    type: 'GET',
+    dataType: 'jsonp',
+	cors: true ,
+	contentType:'application/json',
+	secure: true,
+	headers: {
+    	'Access-Control-Allow-Origin': '*',
+	},
+	beforeSend: function (xhr) {
+   		xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));	
+	}
+}).done(function (data){
+	console.log(true);
+	console.log(data);
+
+	data.each(function(index,element){
+		let article = new Article(0, element.title.val(), element.desc.val());
+		article.addArticle();
+	});
+}).fail(function (data){
+	console.log(false);
+	console.log("l'appel ajax n'a pas fonctionné");
+});
+
+let onglets = $('.onglet');
+
+$(onglets[0]).click(function() {
+	$('form').hide().siblings(':not(form)').show();
+}).focus(function() {
+	$( this ).css('color',ROUGE);
+	$( onglets[1] ).css('color','#000000');
+});
+
+$(onglets[1]).click(function() {
+	$('form').show().siblings(':not(form)').hide();
+	$('table').show();
+}).focus(function() {
+	$( this ).css('color',ROUGE);
+	$( onglets[0] ).css('color','#000000');
+});
